28fc0f9eef016509422032338a8539661aa4dbe1
qzqc <74603939+qzqc@users.noreply.github.com>
2024-11-30 20:45:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,56 @@
-const icons = document.querySelectorAll('.icon');
-const timeLeft = document.getElementById('timeleft');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>qzqc.com.tr contact</title>
+  
+  <!-- Favicon -->
+  <link rel="icon" type="image/png" href="/assets/favicon_io/android-chrome-512x512.png">
+  
+  <!-- Link to cursor-effects library -->
+  <script src="https://unpkg.com/cursor-effects@latest/dist/browser.js"></script>
+  
+  <!-- FontAwesome for icons -->
+  <script src="https://kit.fontawesome.com/36bd361e74.js" crossorigin="anonymous"></script>
 
-icons.forEach(icon => {
-  icon.addEventListener('click', (event) => {
-    event.preventDefault();
-    timeLeft.style.display = "block";
-    let time = 2;
-    const interval = setInterval(() => {
-      time--;
-      timeLeft.innerText = `${time} saniye.`;
-      if (time === 0) {
-        clearInterval(interval);
-        window.location = icon.href;
-      }
-    }, 1000);
-  });
-});
+  <!-- External Stylesheet -->
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <div class="container">
+    <h1>qzqc.com.tr Recording Label email: info@qzqc.com.tr</h1>
+    
+    <div class="icon-container">
+      <a href="https://www.instagram.com/qzqc.com.tr/" target="_blank" class="icon">
+        <i class="fab fa-instagram"></i><span class="icon-text">Instagram</span>
+      </a>
+      <a href="https://www.youtube.com/channel/UCZ5iuF8hiKHbJ5jW_VGHeBg" target="_blank" class="icon">
+        <i class="fab fa-youtube"></i><span class="icon-text">YouTube</span>
+      </a>
+      <a href="https://open.spotify.com/artist/7qep7IJ6UVDm0Hfy271Aqb?si=0PEZEmOgSY2Kn04BaVwEIw" target="_blank" class="icon">
+        <i class="fab fa-spotify"></i><span class="icon-text">Spotify</span>
+      </a>
+      <a href="https://www.paypal.com/donate/?hosted_button_id=92L2TLXE8UG36" target="_blank" class="icon">
+        <i class="fa-solid fa-money-bill"></i><span class="icon-text">Donate</span>
+      </a>
+    </div>
+
+    <div id="timeleft"></div>
+  </div>
+
+  <!-- External JavaScript -->
+  <script src="script.js"></script>
+
+  <script>
+    // Wait for the page to fully load before initializing the effect
+    window.addEventListener("load", () => {
+      // Initialize the Fairy Dust Cursor Effect
+      new cursoreffects.fairyDustCursor({
+        colors: ["#ff0000", "#00ff00", "#0000ff"] // Custom colors for the dust particles
+      });
+    });
+  </script>
+</body>
+</html>
