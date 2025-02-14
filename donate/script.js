80421816ea44df37807df72a187d6f06f4e33d8e
Kaan Sakallı <74603939+qzqc@users.noreply.github.com>
2025-02-14 22:53:11 +0300

    Delete donate/script.js

--- donate/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('.donate-button');
-    buttons.forEach(button => {
-        button.addEventListener('click', function() {
-            alert('Thank you for your support!');
-        });
-    });
-});
